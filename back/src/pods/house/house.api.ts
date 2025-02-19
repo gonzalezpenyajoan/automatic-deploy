4153cbe3d0d4308c7f0414d0c3b22fa2c65a1c39
joan.gonzalez <gonzalezpenyajoan@gmail.com>
2025-02-19 22:32:20 +0100

    fixed string casting bug

--- back/src/pods/house/house.api.ts
+++ back/src/pods/house/house.api.ts
@@ -10,7 +10,7 @@ houseAPI
         try {
             const page = Number(req.query.page);
             const pageSize = Number(req.query.pageSize);
-            const countryCode = String(req.query.countryCode);
+            const countryCode = req.query.countryCode as string;
             const houseList = await houseRepository.getHouseList(page, pageSize, countryCode);
             res.send(mapHouseListFromModelToAPI(houseList));
         } catch (error) {
